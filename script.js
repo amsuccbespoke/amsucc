d46ee5f65e64db4ba5ec63b392e340378e1c863b
amsuccbespoke <fawazabdulkareem111@gmail.com>
2026-01-11 10:33:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+// Hide loading spinner for welcome screen
+document.getElementById('loading-spinner').style.display = 'none';
+
 // Theme Management
 function initTheme() {
   const themeSwitcher = document.getElementById('themeSwitcher');
